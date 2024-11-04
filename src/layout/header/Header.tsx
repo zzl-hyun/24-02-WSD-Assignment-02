c298a045a2a7fec84ac45a8a82010544887e5adf
zzl-hyun <kimgihyun877@gmail.com>
2024-11-04 15:35:33 +0900

    Update setIsMobile Header.tsx

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,13 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { /*faSearch, */faUser, faTicket, faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { faUser, faTicket, faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 import './Header.css';
 
 const Header: React.FC = () => {
   const [isScrolled, setIsScrolled] = useState(false);
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
   const navigate = useNavigate();
+
+    // Detect if the device is mobile based on the User-Agent
+    useEffect(() => {
+      const userAgent = navigator.userAgent || navigator.vendor;
+      setIsMobile(/Mobi|Android/i.test(userAgent));
+    }, []);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -38,39 +45,47 @@ const Header: React.FC = () => {
               <FontAwesomeIcon icon={faTicket} style={{ height: '100%', color: '#E50914' }} />
             </Link>
           </div>
-          <nav className="nav-links desktop-nav">
-            <ul>
-              <li><Link to="/">홈</Link></li>
-              <li><Link to="/popular">대세 콘텐츠</Link></li>
-              <li><Link to="/wishlist">내가 찜한 리스트</Link></li>
-              <li><Link to="/search">찾아보기</Link></li>
-            </ul>
-          </nav>
+          {/* Show desktop navigation only if not on mobile */}
+          {!isMobile && (
+            <nav className="nav-links desktop-nav">
+              <ul>
+                <li><Link to="/">홈</Link></li>
+                <li><Link to="/popular">대세 콘텐츠</Link></li>
+                <li><Link to="/wishlist">내가 찜한 리스트</Link></li>
+                <li><Link to="/search">찾아보기</Link></li>
+              </ul>
+            </nav>
+          )}
         </div>
         <div className="header-right">
           <button className="icon-button" onClick={removeKey}>
             <FontAwesomeIcon icon={faUser} />
           </button>
-          <button className="icon-button mobile-menu-button" onClick={toggleMobileMenu}>
-            <FontAwesomeIcon icon={faBars} />
-          </button>
+          {/* Show mobile menu button only if on mobile */}
+          {isMobile && (
+            <button className="icon-button mobile-menu-button" onClick={toggleMobileMenu}>
+              <FontAwesomeIcon icon={faBars} />
+            </button>
+          )}
         </div>
       </header>
 
       {/* Mobile Navigation */}
-      <div className={`mobile-nav ${isMobileMenuOpen ? 'open' : ''}`}>
-        <button className="close-button" onClick={toggleMobileMenu}>
-          <FontAwesomeIcon icon={faTimes} />
-        </button>
-        <nav>
-          <ul>
-            <li><Link to="/" onClick={toggleMobileMenu}>홈</Link></li>
-            <li><Link to="/popular" onClick={toggleMobileMenu}>대세 콘텐츠</Link></li>
-            <li><Link to="/wishlist" onClick={toggleMobileMenu}>내가 찜한 리스트</Link></li>
-            <li><Link to="/search" onClick={toggleMobileMenu}>찾아보기</Link></li>
-          </ul>
-        </nav>
-      </div>
+      {isMobile && (
+        <div className={`mobile-nav ${isMobileMenuOpen ? 'open' : ''}`}>
+          <button className="close-button" onClick={toggleMobileMenu}>
+            <FontAwesomeIcon icon={faTimes} />
+          </button>
+          <nav>
+            <ul>
+              <li><Link to="/" onClick={toggleMobileMenu}>홈</Link></li>
+              <li><Link to="/popular" onClick={toggleMobileMenu}>대세 콘텐츠</Link></li>
+              <li><Link to="/wishlist" onClick={toggleMobileMenu}>내가 찜한 리스트</Link></li>
+              <li><Link to="/search" onClick={toggleMobileMenu}>찾아보기</Link></li>
+            </ul>
+          </nav>
+        </div>
+      )}
     </div>
   );
 };
