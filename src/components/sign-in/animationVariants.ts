ad37e3146a5992c117ba102e560e05b40c4fa28d
zzl-hyun <kimgihyun877@gmail.com>
2024-11-20 22:04:49 +0900

    v1.0.2

--- src/components/sign-in/animationVariants.ts
+++ src/components/sign-in/animationVariants.ts
@@ -8,8 +8,8 @@ export const logoVariants = {
       pathLength: 1,
       fill: "rgba(229, 16, 19, 1)",
       transition: {
-        default: { delay: 1, duration: 2, ease: [1, 0, 0.8, 1] }, // 약간의 지연 후 자연스럽게 선 애니메이션
-        fill: { delay: 1, duration: 2, ease: [1, 0, 0.8, 1] }, // 경로 애니메이션과 조화를 이루도록 시간 조정
+        default: { delay: 0.5, duration: 3, ease: [1, 0, 0.8, 1] }, // 약간의 지연 후 자연스럽게 선 애니메이션
+        fill: { delay: 0.5, duration: 3, ease: [1, 0, 0.8, 1] }, // 경로 애니메이션과 조화를 이루도록 시간 조정
       },
     },
     hover: {
@@ -29,7 +29,7 @@ export const logoVariants = {
     await boxControls.start({
       opacity: 0,
       scale: 0.8,
-      transition: { duration: 0.5 }, // 로그인 폼이 빠르게 사라짐
+      transition: { duration: 0.2 }, // 로그인 폼이 빠르게 사라짐
     });
   
     // 2. 로그인 폼 숨김 처리
@@ -41,7 +41,7 @@ export const logoVariants = {
       x: "-50%",
       y: "-50%",
       scale: 1.5, // 처음에는 약간만 확대
-      transition: { duration: 2.5 }, // 빠르게 초기 위치로 설정
+      transition: { duration: 3 }, // 빠르게 초기 위치로 설정
     });
   
     // 5. 로고 애니메이션 완료 (선과 채우기 동작)
